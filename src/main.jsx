37d07193900f1091b8070745e310f81bb69be30c
matthew carroll <matthewcarroll@uri.edu>
2025-08-19 22:20:12 -0400

    undo HashRouter for now

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { HashRouter } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
